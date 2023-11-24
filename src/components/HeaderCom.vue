975bea524dd94563fc80028cc970d65b41482d94
lhh <oncwnuFHfKxOgxb4YBUjyQ3Hz75Q@git.weixin.qq.com>
2023-11-25 00:51:17 +0800

    feat: 后台头部

--- src/components/HeaderCom.vue
+++ src/components/HeaderCom.vue
@@ -1,126 +1,61 @@
 <template>
-	<div class="header" >
-    <img src="../assets/a.jpg" style="width:99%;height:268px;"/>
-		<div  style="background-color: white;left:0px;top:30px;width:100%;height:50px;line-height:50px;position: absolute;color: black;">
-      大连旅游网站	<el-button @click="loginout">退出登录{{userName}}</el-button>
+  <div class="header">
+    <div class="title">大连旅游网站</div>
+    <div class="right">
+      <svg viewBox="0 0 1024 1024" width="24">
+        <path
+          fill="#2c3e50"
+          d="M511.626 1.896C229.572 1.896 0.927 230.541 0.927 512.595c0 282.055 228.645 510.699 510.699 510.699s510.698-228.645 510.698-510.699S793.68 1.896 511.626 1.896z m0 69.641c243.606 0 441.058 197.474 441.058 441.058 0 87.347-25.392 168.762-69.194 237.271-73.419-77.609-170.944-132.204-280.597-151.829 70.004-33.755 118.404-105.164 118.404-188.066 0-115.388-93.535-208.922-208.923-208.922S303.452 294.583 303.452 409.97c0 82.902 48.399 154.311 118.403 188.066-110.093 19.704-207.96 74.661-281.479 152.77-44.177-68.704-69.808-150.465-69.808-238.211 0-243.584 197.496-441.058 441.058-441.058z"
+        ></path>
+      </svg>
+      {{ userName }}
+      <div class="logout" @click="loginout">退出登录</div>
     </div>
-	</div>
+    <!-- <img src="../assets/a.jpg" style="width:99%;height:268px;"/> -->
+  </div>
+  <img src="https://dimg04.c-ctrip.com/images/0zg4x120009bcpxsr1A15.jpg" style="width: 100%; height: 268px" />
 </template>
+
 <script setup>
-import { useRouter } from "vue-router"
+import { useRouter } from 'vue-router'
 const Router = useRouter()
-const userName = sessionStorage.getItem("aname")
+const userInfo = JSON.parse(sessionStorage.getItem('admin'))
+const userName = userInfo?.aname ?? ''
+
 const loginout = () => {
-		sessionStorage.removeItem("aname")
-		Router.push("/admin/login");
+  sessionStorage.removeItem('admin')
+  Router.push('/admin/login')
 }
-const myDate = new Date()
-
-const Week = ['日', '一', '二', '三', '四', '五', '六']
-
 </script>
-<style scoped>
+
+<style scoped lang="less">
+img {
+  object-fit: cover;
+}
 .header {
-	position: relative;
-	box-sizing: border-box;
-	width: 100%;
-	height: 60px;
-	font-size: 22px;
-	background: black;
-	color: #fff;
+  background-color: #fff;
+  border-bottom: 1px solid #efefef;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.12), 0 0 3px 0 rgba(0, 0, 0, 0.04);
+  position: sticky;
+  top: 0;
+  padding: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-
-.collapse-btn {
-	float: left;
-	padding: 0 21px;
-	cursor: pointer;
-	line-height: 70px;
+.title {
+  font-weight: 700;
+  font-size: 19px;
 }
-
-.header .logo {
-	float: left;
-	width: 250px;
-	line-height: 70px;
-	font-size: 20px;
-}
-
-.header .mytime {
-	float: left;
-	width: 300px;
-	line-height: 70px;
-	font-size: 15px;
-}
-
-.header-right {
-	float: right;
-	padding-right: 50px;
-}
-
-.header-user-con {
-	display: flex;
-	height: 70px;
-	align-items:center;
-	text-align: right;
-}
-
-.btn-fullscreen {
-	transform: rotate(45deg);
-	margin-right: 5px;
-	font-size: 24px;
-}
-
-.btn-bell,
-.btn-fullscreen {
-	position: relative;
-	width: 30px;
-	height: 30px;
-	text-align: center;
-	border-radius: 15px;
-	cursor: pointer;
-}
-
-.btn-bell-badge {
-	position: absolute;
-	right: 0;
-	top: -2px;
-	width: 8px;
-	height: 8px;
-	border-radius: 4px;
-	background: #f56c6c;
-	color: #fff;
-}
-
-.btn-bell .el-icon-bell {
-	color: #fff;
-}
-
-.user-name {
-	margin-left: 10px;
-}
-
-.user-avator {
-	margin-left: 20px;
-	transition: 0.5s;
-}
-
-.user-avator:hover {
-	transform: rotate(360deg);
-}
-
-.user-avator img {
-	display: block;
-	width: 40px;
-	height: 40px;
-	border-radius: 50%;
-}
-
-.el-dropdown-link {
-	color: #fff;
-	cursor: pointer;
-	margin-left: 5px;
-}
-
-.el-dropdown-menu__item {
-	text-align: center;
+.right {
+  position: absolute;
+  right: 16px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  .logout {
+    margin-left: 11px;
+    cursor: pointer;
+  }
 }
 </style>
--- src/views/admin/MainView.vue
+++ src/views/admin/MainView.vue
@@ -1,43 +1,40 @@
 <template>
-   <div class="common-layout">
+  <div class="common-layout">
+    <HeaderCom></HeaderCom>
     <el-container>
-      <el-header> <HeaderCom></HeaderCom> </el-header>
-      <br>  <br>  <br>  <br>  <br>  <br>  <br>  <br>  <br>
-      <el-container style="padding-top: 30px;height: 100vh;">
-        <el-aside width="240px">  <AsideCom/>     </el-aside>
-        <el-main style="background-color: white;">
-             <router-view></router-view>
+      <el-header></el-header>
+      <el-container>
+        <el-aside width="240px"> <AsideCom /> </el-aside>
+        <el-main style="background-color: white">
+          <router-view></router-view>
         </el-main>
       </el-container>
     </el-container>
   </div>
 </template>
 <script>
-import { reactive, toRefs } from "vue";
+import { reactive, toRefs } from 'vue'
 import HeaderCom from '@/components/HeaderCom.vue'
 import AsideCom from '@/components/AsideCom.vue'
 export default {
-  name: "type",
-  components:{
+  name: 'type',
+  components: {
     HeaderCom,
     AsideCom,
   },
   setup() {
     //页面初始化方法
     const data = reactive({
-      name: "大红",
-    });
-    const login = () => {};
+      name: '大红',
+    })
+    const login = () => {}
 
     // 所有的变量和方法，需要在return返回，外界才能访问
     return {
       ...toRefs(data),
       login,
-     };
+    }
   },
-};
+}
 </script>
-<style scoped>
-
-</style>
-
+<style scoped></style>
